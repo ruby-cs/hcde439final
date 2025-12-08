6726fd190896aba547c2dfbc4a2ee6415e9e91b3
ruby-cs <18ruby.cs@gmail.com>
2025-12-08 03:02:36 -0800

    edited joystick control, changed "mouse" to red led, "cat" is blue led, added level speed and data between arduino and website, added speed of red LED

--- LED Trap/sketch.ino
+++ LED Trap/sketch.ino
@@ -1,89 +1,140 @@
+// Adafruit Neopixel library for WS2818 RGB LEDs
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN 6
+// RGB LED pin and count
+#define LED_PIN   6
 #define LED_COUNT 64
 
+// Joystick pins
 #define xPin A0
 #define yPin A1
 #define swPin 2
 
+// Number of rows and columns
 const int ROWS = 8;
 const int COLS = 8;
 
-Adafruit_Neopixel strip (LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-int redX = 0;
-int redY = 0;
-
+// "Cat" (player, blue LED) position
 int blueX = 0;
 int blueY = 0;
 
-unsigned long lastBlueTime = 0;
-const unsigned long blueInterval = 2000;
+// "Mouse" (red LED) position
+int redX = 0;
+int redY = 0;
 
+// Speed interval for red LED
+unsigned long redInterval = 2000;
+unsigned long lastRedTime = 0;
+
+// Joystick calibration
+int centerX = 512;
+int centerY = 512;
+int deadzone = 80;
+
+// Converting X and Y to LED index
 int xyToIndex(int x, int y) {
-  if (y % 2 == 0) {
-    return y * COLS + x;
-  } else {
-    return y * COLS + (COLS - 1 - x);
-  }
+  if (y % 2 == 0) return y * COLS + x;
+  return y * COLS + (COLS - 1 - x);
 }
 
 void setup() {
+  Serial.begin(115200);
+
+  // LED strip power and brightness
   strip.begin();
   strip.show();
   strip.setBrightness(50);
 
+  // Joystick inputs
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
-
   pinMode(swPin, INPUT_PULLUP);
-
+  
+  // For random LED positions
   randomSeed(analogRead(A2));
+
+  // Calibrating joystick center
+  long joyX = 0, joyY = 0;
+  for (int i = 0; i < 40; i++) {
+    joyX += analogRead(xPin);
+    joyY += analogRead(yPin);
+    delay(5);
+  }
+  centerX = joyX / 40;
+  centerY = joyY / 40;
+}
+
+void readSerialInput() {
+  // Level data between Arduino and website
+  if (!Serial.available()) return;
+  String s = Serial.readStringUntil('\n');
+  s.trim();
+  int levelReceived = s.toInt();
+
+  // Increase speed of red LED each level (max 300 ms)
+  if (levelReceived >= 1 && levelReceived <= 20) {
+    redInterval = max(300, 2000 - (levelReceived - 1) * 150);
+    Serial.print(levelReceived);
+    Serial.println(redInterval);
+  }
 }
 
 void loop() {
+  readSerialInput();
+
+  // Joystick values
   int xVal = analogRead(xPin);
-  int yVal = analogRead(yPIn);
+  int yVal = analogRead(yPin);
+  bool pressed = (digitalRead(swPin) == LOW);
 
-  if (xVal < 300 && redX > 0) {
-    redX--;
-    delay(120);
-  }
-  if (xVal > 700 && redX < COLS -1) {
-    redX++;
-    delay(120);
-  }
-  if (yVal > 700 && redY > 0) {
-    redY--;
-    delay(120);
-  }
-  if (yVal < 300 && redY < ROWS -1) {
-    redY++;
-    delay(120);
+  int lowX  = centerX - deadzone;
+  int highX = centerX + deadzone;
+  int lowY  = centerY - deadzone;
+  int highY = centerY + deadzone;
+
+  // Player movement control
+  static unsigned long lastMove = 0;
+  if (millis() - lastMove > 70) {
+    if (xVal < lowX && blueX > 0) blueX--;
+    if (xVal > highX && blueX < COLS - 1) blueX++;
+    if (yVal > highY && blueY > 0) blueY--;
+    if (yVal < lowY && blueY < ROWS - 1) blueY++;
+    lastMove = millis();
   }
 
-  if (millis() - lastBlueTime >= blueInterval) {
-    blueX = random(0, COLS);
-    blueY = random(0, ROWS);
-    lastBlueTime = millis();
+  // Move red LED to random position at interval speed
+  if (millis() - lastRedTime >= redInterval) {
+    redX = random(0, COLS);
+    redY = random(0, ROWS);
+    lastRedTime = millis();
   }
 
   strip.clear();
 
+  // Red and blue index
+  int redIndex  = xyToIndex(redX,  redY);
   int blueIndex = xyToIndex(blueX, blueY);
-  int redINdex = xyToIndex(redX, redY);
 
+  // Collision if red and blue LEDs are in the same position
   bool collision = (redX == blueX && redY == blueY);
-  bool pressed = (digitalRead(swPin) == LOW);
 
+  // Collision colors
   if (collision && pressed) {
-    strip.setPixelColor(redIndex, strip.Color(0, 255, 0));
-  } else if (collision) {
-    strip.setPixelColor(redIndex, strip.Color(200, 50, 0));
-  } else {
-    strip.setPixelCOlor(blueIndex, strip.Color(255, 0, 0));
+    // If there's collision and sw is pressed, green LED
+    strip.setPixelColor(blueIndex, strip.Color(0, 255, 0));
+    Serial.println("caught");
+  }
+  else if (collision) {
+    // If there's collision, yellow LED
+    strip.setPixelColor(blueIndex, strip.Color(255, 255, 0));
+  }
+  else {
+    // Blue LED is player, red LED is "mouse"
+    strip.setPixelColor(blueIndex, strip.Color(0, 0, 255));
+    strip.setPixelColor(redIndex, strip.Color(255, 0, 0));
   }
 
-  strip.show():
+  strip.show();
 }
